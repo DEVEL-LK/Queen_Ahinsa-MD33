69e5e381ad4644b9176311c09afec6ed35962dca
DEVEL-LK <gimshandilisha578@gmail.com>
2025-11-16 14:06:52 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "2JlwmATD#zLegNqDof9o5a9R08pD-_eaz35j3zTtGQaV_BNEuhOY",
-MONGODB: process.env.MONGODB || "mongodb://mongo:MgRxoQTXWnKFtprOrocHAeucSEKWBtoE@junction.proxy.rlwy.net:45585",
+SESSION_ID: process.env.SESSION_ID || "DVFxVCLI#MHIEh8jQxPiuoW1KzcsyH9oZVbDHCSUbnpAMlOSkdfo",
+MONGODB: process.env.MONGODB || "redis://default:FAZZpqvqGECruYnREOBIIhboDAUKnGqM@hopper.proxy.rlwy.net:25469",
 };
